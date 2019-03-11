aa3b63f5a2156284e313845ee1945546f3a6cabd
Florian Rechberger <florianrechberger@gmx.at>
2019-03-11 11:50:46 +0100

    add exercise5 -loops with fruits

---
+++ exercise5.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>In love with your ego</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<h1>Fruit Chaos!</h1>
+		<?php
+			$fruits = array( "Apple", "Banana", "Mango", "Pineapple", "Cherry", "Tomato", "Kiwi", "Grapefruit", "Orange", "Grape", "Berries");
+			foreach( $fruits as $value )
+			{
+			echo "The fruit is: $value <br />";
+			}
+		?>
+		</body>
+</html>
+
