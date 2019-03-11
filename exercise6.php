63a5bd98fa1650157a6d958d49f32b1981e08d1c
Simon Blaha <s.blaha@live.at>
2019-03-11 14:12:54 +0100

    upload exercise6

--- exercise6.php
+++ exercise6.php
@@ -8,20 +8,25 @@
 	<?php  
 
 		$heroes = array 
-		
+
 		(
 
-			array("Ana", "DPS"),
-			array("Bastion, DPS"),
-			array("Mercy" "Healer"),
+			array("Ana", "Healer"),
+			array("Bastion", "DPS"),
+			array("Mercy", "Healer"),
 			array("Lucio", "Healer"),
-			array("Reinardt", "Tank"),
+			array("Reinhardt", "Tank"),
 			array("Zarya", "Tank"),
 			array("Genji", "DPS")
 
 		);
 
-		//echo "The heroe is: ".$heroes[0][0]." and the class is ".$heroes[0][1].".<br>";
+		echo "The heroe is: ".$heroes[0][0]." and the class is ".$heroes[0][1].".<br>";
+		echo "The heroe is: ".$heroes[1][0]." and the class is ".$heroes[1][1].".<br>";
+		echo "The heroe is: ".$heroes[2][0]." and the class is ".$heroes[2][1].".<br>";
+		echo "The heroe is: ".$heroes[4][0]." and the class is ".$heroes[4][1].".<br>";
+
+
 
 	?>
 
