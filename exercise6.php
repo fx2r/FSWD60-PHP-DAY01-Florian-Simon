ae1fcce63ae5ea0dc6a1cee9cdc3d0df1efe7184
Simon Blaha <s.blaha@live.at>
2019-03-11 14:06:28 +0100

    upload exercise6

---
+++ exercise6.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<?php  
+
+		$heroes = array 
+		
+		(
+
+			array("Ana", "DPS"),
+			array("Bastion, DPS"),
+			array("Mercy" "Healer"),
+			array("Lucio", "Healer"),
+			array("Reinardt", "Tank"),
+			array("Zarya", "Tank"),
+			array("Genji", "DPS")
+
+		);
+
+		//echo "The heroe is: ".$heroes[0][0]." and the class is ".$heroes[0][1].".<br>";
+
+	?>
+
+</body>
+</html>
